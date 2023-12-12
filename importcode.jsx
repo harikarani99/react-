1f9ce4a4778b004021b8002c5d09ebdf3fc26c6f
Harika rani <151608700+harikarani99@users.noreply.github.com>
2023-12-12 14:34:31 +0530

    Add files via upload

---
+++ importcode.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import {person} from './export'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+console.log(person);
